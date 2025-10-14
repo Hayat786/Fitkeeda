63e3bc209daf598101059b02edc76c26f9e1ba71
AthulKrishna0010 <krishhhnnaa007@gmail.com>
2025-10-14 17:28:22 +0530

    backend integrated

--- fitkeeda-ui/src/utils/api.ts
+++ fitkeeda-ui/src/utils/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:8080"; // change to your backend URL
+const API_BASE_URL = "https://fitkeeda-nest.onrender.com"; // change to your backend URL
 
 const api = axios.create({
   baseURL: API_BASE_URL,
